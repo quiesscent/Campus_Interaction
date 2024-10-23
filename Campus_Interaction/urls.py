ae6099fcf16a1bba1d874cdae79bad475a26860f
fideleliud <fideleliudclimax@gmail.com>
2024-10-23 09:52:17 +0300

    enhance the templates

--- Campus_Interaction/urls.py
+++ Campus_Interaction/urls.py
@@ -2,23 +2,24 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.views.static import serve
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('core.urls')),
     path("profile/", include("profiles.urls")),
-] 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
-if not settings.DEBUG:
-    urlpatterns += [
-        re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
-        re_path(
-            r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}
-        ),
-    ]
-else:
-    from django.conf.urls.static import static
+# if not settings.DEBUG:
+#     urlpatterns += [
+#         re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+#         re_path(
+#             r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}
+#         ),
+#     ]
+# else:
+#     from django.conf.urls.static import static
 
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
