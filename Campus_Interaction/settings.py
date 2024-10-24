fc66a4d1672442f47516e535253eb8cfbe1133e4
Demapis <161503068+Demapis@users.noreply.github.com>
2024-10-24 20:02:57 +0300

    Update settings.py

--- Campus_Interaction/settings.py
+++ Campus_Interaction/settings.py
@@ -12,7 +12,6 @@ ALLOWED_HOSTS = ["SITE_URL", "0.0.0.0", "127.0.0.1", "localhost"]
 
 
 INSTALLED_APPS = [
-<<<<<<< HEAD
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -27,25 +26,7 @@ INSTALLED_APPS = [
     'channels',
     "messaging",
      ]
-    
 
-=======
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "social_django",
-    "marketplace",
-    "profiles",
-    "core",
-    "messaging",
-    "notifications",
-    "events",
-    "polls"
-]
->>>>>>> 5a7ebca82065f966c5a4e11930b8eb142403f780
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
