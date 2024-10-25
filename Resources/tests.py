2910785147c10d5c969a1ae0928faf1ce4f47880
fideleliud <fideleliudclimax@gmail.com>
2024-10-25 23:00:19 +0300

    resolve issues

--- Resources/tests.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-from django.test import TestCase
-
-# Create your tests here.
-=======
-from django.test import TestCase
-
-# Create your tests here.
->>>>>>> 7cc8ac5 (uploaded the resources app)
