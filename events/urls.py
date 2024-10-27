78d9cd2db21e1a296446844e8155324eb1835150
Benard Karanja <benardopeter4@gmail.com>
2024-10-27 16:36:31 +0300

    Eliminated redundant imports

--- events/urls.py
+++ events/urls.py
@@ -1,31 +1,3 @@
-# from django.urls import path, include
-# from . import views
-# from .views import university_autocomplete
-
-# app_name = 'events'
-
-# urlpatterns = [
-#     # Main event views
-#     path('', views.event_list, name='event_list'),
-#     path('<int:event_id>/', views.event_detail, name='event_detail'),
-#     path('create/', views.create_event, name='create_event'),  # Event creation
-
-#     # Event actions
-#     path('<int:event_id>/react/', views.toggle_reaction, name='toggle_reaction'),  # Event reactions
-#     path('api/university-autocomplete/', views.university_autocomplete, name='university_autocomplete'),
-
-#     # Comment-related endpoints
-#     path('api/events/<int:event_id>/comment/', views.add_comment, name='add_comment'),  # Add a comment to an event
-#     path('api/comments/<int:comment_id>/like/', views.toggle_comment_like, name='toggle_comment_like'),  # Like a comment
-
-#     # Event delete API endpoint
-#     path('api/events/<int:event_id>/delete/', views.delete_event, name='delete_event'),  # Delete an event
-
-#     # Integrate select2 for dropdowns
-#     path('select2/', include('django_select2.urls')),
-# ]
-
-
 # events/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
