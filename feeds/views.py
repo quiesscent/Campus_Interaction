3cc8780c4820b9d4415cb81257533a1b7811a9f9
fideleliud <fideleliudclimax@gmail.com>
2024-10-27 18:32:55 +0300

    minor changes

--- feeds/views.py
+++ feeds/views.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
