082d1ab9d5f96b6e879a3c099b355f5d77f7d620
mohpee <mohpee@kali>
2024-10-30 12:03:16 +0300

    landmark button,searching and updating your current location but needs an API

--- maps/models.py
+++ maps/models.py
@@ -1,3 +1,11 @@
+
 from django.db import models
 
-# Create your models here.
+class Landmark(models.Model):
+    name = models.CharField(max_length=100)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
