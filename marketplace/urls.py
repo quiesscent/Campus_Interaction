e13dfb5aa906bc5bdf2c08c32920b5a9ce796fe9
audo-wrld <audo-wrld@kali>
2024-11-01 12:59:10 +0300

    Links to market place added in the system

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
+app_name = "marketplace"
 urlpatterns = [
     path('', views.item_list, name='item_list'),
     path('item/<int:item_id>/', views.item_detail, name='item_detail'),
