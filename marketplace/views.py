b6cb44434aec8c327f90bb60abc9291d3ef239fa
audo-wrld <audo-wrld@kali>
2024-11-18 16:06:18 +0300

    All Functions for marketplace including adding items, updates etc.

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,154 +1,475 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Item, UserRating, Category, SearchHistory
+from .models import (
+    Item,
+    UserRating,
+    Category,
+    SearchHistory,
+    Like,
+    Notification,
+    ItemImage,
+    Cart,
+    CartItem,
+)
 from .forms import ItemForm, RatingForm
 from django.shortcuts import render
-from django.db.models import Q  # Import Q for complex queries
+from django.db.models import Q, F  # Import Q for complex queries
 from .models import Item
-from django.contrib import messages 
-from django.db.models import F 
+from django.contrib import messages
+from django.db.models import F
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.db.models import Sum
+from django.utils import timezone
+from .models import Item
+import json
+from django.http import JsonResponse
+import calendar
+from django.views.decorators.http import require_POST
+from datetime import timedelta
+from django.db.models import Avg
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
 
 
+from django.db.models import F, Count, Q
+
+
+# All Items
 def item_list(request):
-    query = request.GET.get('q')
-    category_name = request.GET.get('category', 'all')  # Default to 'all' if no category is selected
-
-    # Start with all items
+    query = request.GET.get("q")
     items = Item.objects.all()
 
-    # Filter items based on the search query in title or description
+    # Filter items if there is a search query
     if query:
         items = items.filter(
             Q(title__icontains=query) | Q(description__icontains=query)
         )
 
-    # Filter by category name if specified and not "all"
-    if category_name and category_name != "all":
-        items = items.filter(category__name__iexact=category_name)
+    # Get the IDs of items liked by the current user
+    liked_items = set(
+        Like.objects.filter(user=request.user).values_list("item_id", flat=True)
+    )
+    # Assuming the structure: CartItem -> Cart -> User
+    cart_count = CartItem.objects.filter(
+        cart__user=request.user, item__sold=False
+    ).count()
 
-    # Get all categories for the dropdown menu
+    # Check if the request is an AJAX request
+    if request.headers.get("x-requested-with") == "XMLHttpRequest":
+        items_data = [
+            {
+                "id": item.id,
+                "title": item.title,
+                "price": item.price,
+                "image_url": item.image.url,
+                "category": item.category.name,
+                "sold": item.sold,
+                "liked": item.id in liked_items,
+                "total_likes": item.total_likes,
+                "original_price": item.original_price,
+                "new_price": item.new_price,
+            }
+            for item in items
+        ]
+        return JsonResponse({"items": items_data})
+
+    # For non-AJAX requests (initial load)
     categories = Category.objects.all()
+    deal_items = Item.objects.filter(
+        new_price__lt=F("original_price"),
+        new_price__isnull=False,
+        original_price__isnull=False,
+        sold=False,
+    )
 
-    # Initialize search history and search items
-    search_items = []
-    if request.user.is_authenticated:
-        search_history = SearchHistory.objects.filter(user=request.user).order_by('-created_at')
+    # Feature items - Items where the new price is less than the original price, and both prices are not null, and not sold.
+    feature_items = Item.objects.filter(
+        new_price__lt=F("original_price"),
+        new_price__isnull=False,
+        original_price__isnull=False,
+        sold=False,
+    )[:3]
 
-        # Create a list of tuples (search_history, item) for each search
-        for search in search_history:
-            # Look for an item that matches the search query
-            item = items.filter(title__iexact=search.query).first()
-            search_items.append((search, item))
+    # Hot trend items - Items with at least 2 likes, not sold.
+    hot_trend_items = Item.objects.annotate(num_likes=Count("likes")).filter(
+        num_likes__gte=2, sold=False
+    )[:3]
 
-    # Create a dictionary to categorize items by category
-    categorized_items = {}
-    for category in categories:  # Ensure we iterate over all categories
-        categorized_items[category.name] = items.filter(category=category)
-    deal_items = Item.objects.filter(price__lt=F('original_price'))
+    best_seller_sellers = (
+        Item.objects.filter(sold=True)
+        .values("seller")
+        .annotate(sold_count=Count("id"))
+        .filter(sold_count__gte=2)
+        .values_list("seller", flat=True)
+    )
 
-    return render(request, 'marketplace/item_list.html', {
-        'categorized_items': categorized_items,  # Pass the categorized items
-        'query': query,
-        'categories': categories,
-        'selected_category': category_name,
-        'search_items': search_items,  # Pass the matched items
-        'user': request.user,
-        'deal_items': deal_items,
-    })
+    best_seller_items = Item.objects.filter(seller__in=best_seller_sellers, sold=False)[
+        :3
+    ]
+
+    return render(
+        request,
+        "marketplace/item_list.html",
+        {
+            "items": items,
+            "categories": categories,
+            "deal_items": deal_items,
+            "feature_items": feature_items,  # Features
+            "hot_trend_items": hot_trend_items,
+            "best_seller_items": best_seller_items,
+            "liked_items": liked_items,  # Pass liked items to the template
+            "cart_count": cart_count,
+        },
+    )
 
 
 def item_detail(request, item_id):
     item = get_object_or_404(Item, id=item_id)
-
-    # Initialize form
     form = RatingForm()
 
-    if request.method == 'POST' and request.user.is_authenticated:
-        stars = request.POST.get('stars')
-        comment = request.POST.get('comment')  # Capture comment if needed
-        form = RatingForm(request.POST)  # Create a form instance with POST data
+    # Get the ratings and count them
+    reviews = item.ratings.all()  # Retrieve all reviews for the item
+    review_count = reviews.count()  # Count the number of reviews
 
-        # Check if the form is valid and stars rating is provided
+    # If user submits a review
+    if request.method == "POST" and request.user.is_authenticated:
+        stars = request.POST.get("stars")
+        comment = request.POST.get("comment")
+        form = RatingForm(request.POST)
+
         if form.is_valid() and stars:
-            # Create or update the user rating
             user_rating, created = UserRating.objects.get_or_create(
                 item=item,
                 user=request.user,
-                defaults={'stars': stars, 'comment': comment}  # Create if not exists
+                defaults={"stars": stars, "comment": comment},
             )
 
-            if not created: 
-                # Update existing rating
+            if not created:
                 user_rating.stars = stars
-                user_rating.comment = comment  
+                user_rating.comment = comment
                 user_rating.save()
 
-            # Redirect to the same item detail page after saving
-            return redirect('item_detail', item_id=item.id)
+            return redirect("marketplace:item_detail", item_id=item.id)
 
-    # Create a new search history entry with the item title regardless of the source
+    # Retrieve related items in the same category, excluding the current item
+    related_items = Item.objects.filter(category=item.category).exclude(id=item_id)[
+        :4
+    ]  # limit to 4 items
+    liked_items = set(
+        Like.objects.filter(user=request.user).values_list("item_id", flat=True)
+    )
+
     if request.user.is_authenticated:
         SearchHistory.objects.get_or_create(user=request.user, query=item.title)
 
-    return render(request, 'marketplace/item_detail.html', {
-        'item': item,
-        'form': form,
-        'average_rating': item.average_rating
-    })
+    return render(
+        request,
+        "marketplace/item_detail.html",
+        {
+            "item": item,
+            "form": form,
+            "reviews": reviews,
+            "review_count": review_count,
+            "average_rating": item.average_rating,
+            "related_items": related_items,
+            "liked_items": liked_items,
+        },
+    )
+
+
+@login_required
 def add_item(request):
-    if request.method == 'POST':
+    error_message = None  # Initialize error_message
+
+    if request.method == "POST":
         form = ItemForm(request.POST, request.FILES)
+
+        # Check if the image field is missing
+        if "image" not in request.FILES or not request.FILES["image"]:
+            error_message = (
+                "Image required."  # Set the error message if image is missing
+            )
+            return render(
+                request,
+                "marketplace/add_item.html",
+                {"form": form, "error_message": error_message},
+            )
+
+        # Form validation
         if form.is_valid():
             item = form.save(commit=False)
             item.seller = request.user
             item.save()
-            return redirect('item_list')
+            messages.success(request, "Your item has been successfully listed!")
+            return redirect("marketplace:item_list")
+        else:
+            # If the form has other validation errors, we can add a general error message
+            messages.error(
+                request,
+                "There were errors in your submission. Please correct them below.",
+            )
+            return render(request, "marketplace/add_item.html", {"form": form})
+
     else:
         form = ItemForm()
-    return render(request, 'marketplace/add_item.html', {'form': form})
+
+    return render(
+        request,
+        "marketplace/add_item.html",
+        {"form": form, "error_message": error_message},
+    )
 
 
+@login_required
 def seller_dashboard(request):
     if request.user.is_authenticated:
         user_items = Item.objects.filter(seller=request.user)
 
-        if request.method == 'POST':
-            item_id = request.POST.get('item_id')
-            new_price = request.POST.get('new_price')
-            item = get_object_or_404(Item, id=item_id, seller=request.user)
+        # Collect data for the sales graph
+        months = []
+        total_sales = []
 
-            # Update price
-            if new_price:
-                item.original_price = item.price  # Set the current price as the original price
-                item.price = new_price
-                item.save()
-                messages.success(request, f"Price for {item.title} updated successfully.")
-                return redirect('seller_dashboard')
+        # Get sales data grouped by month
+        sales_data = (
+            Item.objects.filter(seller=request.user, sold=True)
+            .values("sold_date__month")
+            .annotate(total_sales=Sum("price"))
+            .order_by("sold_date__month")
+        )
 
-        return render(request, 'marketplace/seller_dashboard.html', {
-            'user_items': user_items,
-        })
+        # Prepare the data for JavaScript
+        for data in sales_data:
+            month_name = calendar.month_name[data["sold_date__month"]]
+            months.append(month_name)
+            total_sales.append(data["total_sales"])
+
+        # Fetch unread notifications for the seller without marking as read
+        unread_notifications = Notification.objects.filter(
+            recipient=request.user,
+            is_read=False,
+            created_at__gte=timezone.now() - timedelta(hours=24),
+        )
+
+        # Determine which products are popular (more than 1 like)
+        popular_items = []
+        for item in user_items:
+            if item.likes.count() > 10 and not item.sold:
+                popular_items.append(item)
+
+        return render(
+            request,
+            "marketplace/seller_dashboard.html",
+            {
+                "user_items": user_items,
+                "months": json.dumps(months),
+                "total_sales": json.dumps(total_sales),
+                "unread_notifications": unread_notifications,
+                "popular_items": popular_items,  # Pass the popular items to the template
+            },
+        )
     else:
-        return redirect('login')
+        return redirect("login")
+
 
 def mark_as_sold(request, item_id):
     item = get_object_or_404(Item, id=item_id)
-    if request.method == 'POST' and request.user == item.seller:
+    if request.method == "POST" and request.user == item.seller:
         item.sold = True
         item.save()
         messages.success(request, "Item marked as sold.")
-        return redirect('seller_dashboard')
-    return redirect('item_detail', item_id=item.id)
+        return redirect("marketplace:seller_dashboard")
+    return redirect("marketplace:item_detail", item_id=item.id)
+
 
 def delete_item(request, item_id):
+    success_message = None
     item = get_object_or_404(Item, id=item_id)
 
-    if request.method == 'POST' and request.user == item.seller:
+    if request.method == "POST" and request.user == item.seller:
         item.delete()
-        messages.success(request, "Item deleted.")
+        success_message = "Item deleted successfully"
         if not Item.objects.filter(seller=request.user).exists():
-            return redirect('item_list')  
+            return redirect("item_list")
 
-        return redirect('seller_dashboard') 
+        return redirect("marketplace:seller_dashboard")
 
-    return redirect('item_detail', item_id=item.id)
+    return redirect("marketplace:item_detail", item_id=item.id)
+
+
+@require_POST
+def like_item(request, item_id):
+    item = Item.objects.get(id=item_id)
+    user = request.user
+    like, created = Like.objects.get_or_create(user=user, item=item)
+
+    if not created:
+        # If the item was already liked, the user is un liking it
+        like.delete()
+        liked = False
+    else:
+        liked = True
+        if user != item.seller:
+            Notification.objects.create(
+                recipient=item.seller,
+                item=item,
+                message=f"{user.username} is interested in your product '{item.title}'.",
+            )
+
+    return JsonResponse(
+        {
+            "liked": liked,
+            "total_likes": item.likes.count(),
+        }
+    )
+
+
+# The view to display the form for updating an item
+def update_item_view(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+
+    if request.method == "POST":
+        item.title = request.POST.get("title")
+        item.description = request.POST.get("description")
+        item.price = request.POST.get("price")
+
+        # Check if new_price is not empty, otherwise set to None
+        new_price = request.POST.get("new_price", None)
+        if new_price == "":
+            new_price = None
+        item.new_price = new_price
+
+        item.specification = request.POST.get("specification", None)
+        item.brand = request.POST.get("brand", None)
+
+        if "image" in request.FILES:
+            item.image = request.FILES["image"]
+
+        if "additional_images" in request.FILES:
+            for img in request.FILES.getlist("additional_images"):
+                ItemImage.objects.create(item=item, image=img)
+
+        item.save()
+        return redirect("marketplace:item_detail", item_id=item.id)
+
+    return render(request, "marketplace/update_item.html", {"item": item})
+
+
+@require_http_methods(["POST"])
+def rate_item(request, item_id):
+    if not request.user.is_authenticated:
+        return JsonResponse(
+            {"error": "You must be logged in to rate items"}, status=403
+        )
+
+    item = get_object_or_404(Item, id=item_id)
+
+    try:
+        data = json.loads(request.body)
+        stars = data.get("stars")
+        comment = data.get("comment", "")
+
+        if not stars or not isinstance(stars, (int, str)) or not (1 <= int(stars) <= 5):
+            return JsonResponse({"error": "Invalid star rating"}, status=400)
+
+        stars = int(stars)
+
+        user_rating, created = UserRating.objects.update_or_create(
+            item=item, user=request.user, defaults={"stars": stars, "comment": comment}
+        )
+
+        average_rating = UserRating.objects.filter(item=item).aggregate(Avg("stars"))[
+            "stars__avg"
+        ]
+        average_rating = round(float(average_rating or 0), 2)
+
+        return JsonResponse(
+            {
+                "success": True,
+                "average_rating": average_rating,
+                "message": "Rating submitted successfully",
+            }
+        )
+
+    except json.JSONDecodeError:
+        return JsonResponse({"error": "Invalid JSON data"}, status=400)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
+
+
+# Add to Cart
+@login_required
+def add_to_cart(request, item_id):
+    if request.method == "POST":
+        item = get_object_or_404(Item, id=item_id)
+        cart, created = Cart.objects.get_or_create(user=request.user)
+
+        # Check if the item is already in the cart
+        if CartItem.objects.filter(cart=cart, item=item).exists():
+            return JsonResponse(
+                {
+                    "status": "exists",
+                    "message": "Item already in cart",
+                }
+            )
+
+        # If item is not in the cart, add it
+        CartItem.objects.create(cart=cart, item=item, quantity=1)
+        return JsonResponse(
+            {
+                "status": "success",
+                "message": "Item added to cart",
+            }
+        )
+
+    return JsonResponse(
+        {"status": "error", "message": "Authentication required"}, status=403
+    )
+
+
+def cart_page(request):
+    # Retrieve the cart items for the logged-in user
+    cart_items = CartItem.objects.filter(cart__user=request.user)
+
+    # Calculate the total price of the items in the cart, excluding sold items
+    total_price = sum(
+        item.item.price * item.quantity for item in cart_items if not item.item.sold
+    )
+
+    # Calculate the cart count excluding sold items
+    cart_count = CartItem.objects.filter(
+        cart__user=request.user, item__sold=False
+    ).count()
+
+    # Pass the cart items, total price, and count to the template
+    context = {
+        "cart_items": cart_items,
+        "total_price": total_price,
+        "cart_count": cart_count,
+    }
+    return render(request, "marketplace/cart_page.html", context)
+
+
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def remove_cart_item(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        item_id = data.get("item_id")
+
+        # Retrieve and delete the cart item for the user
+        cart_item = CartItem.objects.get(id=item_id, cart__user=request.user)
+        cart_item.delete()
+
+        # Calculate the new total and item count for the user's cart
+        user_cart_items = CartItem.objects.filter(cart__user=request.user)
+        new_total = sum(item.item.price * item.quantity for item in user_cart_items)
+        cart_count = user_cart_items.count()
+
+        return JsonResponse(
+            {"success": True, "new_total": new_total, "cart_count": cart_count}
+        )
+
+    return JsonResponse({"success": False})
