acb738fbbbc2b596189929b9d9eda736be25af75
audo-wrld <audo-wrld@kali>
2024-11-18 16:05:31 +0300

    All links related to marketplace

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -10,4 +10,11 @@ urlpatterns = [
     path('dashboard/', views.seller_dashboard, name='seller_dashboard'),
     path('item/<int:item_id>/mark_as_sold/', views.mark_as_sold, name='mark_as_sold'),
     path('item/<int:item_id>/delete/', views.delete_item, name='delete_item'), 
+    path('items/', views.item_list, name='item_search'),  
+    path('items/<int:item_id>/like/', views.like_item, name='like_item'),
+    path('item/update/<int:item_id>/', views.update_item_view, name='update_item'),
+    path('items/<int:item_id>/rate/', views.rate_item, name='rate_item'),
+    path('items/<int:item_id>/add_to_cart/', views.add_to_cart, name='add_to_cart'),
+    path('cart/', views.cart_page, name='cart_page'),
+    path('remove-cart-item/', views.remove_cart_item, name='remove_cart_item'),
 ]
